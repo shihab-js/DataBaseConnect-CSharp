f6baea8e9f630bc49f19eefcb36429a4661774ed
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-25 15:50:19 +0600

    Design completed and System.Data.SqlClient Installed....

--- DatabaseConnect/Form1.Designer.cs
+++ DatabaseConnect/Form1.Designer.cs
@@ -29,20 +29,35 @@ namespace DatabaseConnect
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(96, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Connection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(513, 360);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Database Connection";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DatabaseConnect/Form1.cs
+++ DatabaseConnect/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DatabaseConnect
 {
@@ -17,5 +18,9 @@ namespace DatabaseConnect
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
